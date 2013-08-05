f03a498b29d2bb2b2c177c6acfa0af3ee9b79117
Harry Atkins <harryatkins@me.com>
2013-08-05 13:02:34 +0200

    New Design

---
+++ js/scripts.js
@@ -0,0 +1,150 @@
+$(document).ready(function () {  
+
+
+
+$('#services .service:nth-child(1) .service-icon').click(function() {
+  $(this).animate({
+    left: '-=250',
+  }, 200, function() {
+  });
+  $('#services .service:nth-child(1) .service-description').animate({
+    left: '-=350',
+  }, 200, function() {
+  });
+  return false;
+});
+
+$('#services .service:nth-child(1) .service-description').click(function() {
+  $(this).animate({
+    left: '+=350',
+  }, 200, function() {
+  });
+  $('#services .service:nth-child(1) .service-icon').animate({
+    left: '+=250',
+  }, 200, function() {
+  });
+  return false;
+});
+
+$('#services .service:nth-child(2) .service-icon').click(function() {
+  $(this).animate({
+    left: '-=250',
+  }, 200, function() {
+  });
+  $('#services .service:nth-child(2) .service-description').animate({
+    left: '-=350',
+  }, 200, function() {
+  });
+  return false;
+});
+
+$('#services .service:nth-child(2) .service-description').click(function() {
+  $(this).animate({
+    left: '+=350',
+  }, 200, function() {
+  });
+  $('#services .service:nth-child(2) .service-icon').animate({
+    left: '+=250',
+  }, 200, function() {
+  });
+  return false;
+});
+
+$('#services .service:nth-child(3) .service-icon').click(function() {
+  $(this).animate({
+    left: '-=250',
+  }, 200, function() {
+  });
+  $('#services .service:nth-child(3) .service-description').animate({
+    left: '-=350',
+  }, 200, function() {
+  });
+  return false;
+});
+
+$('#services .service:nth-child(3) .service-description').click(function() {
+  $(this).animate({
+    left: '+=350',
+  }, 200, function() {
+  });
+  $('#services .service:nth-child(3) .service-icon').animate({
+    left: '+=250',
+  }, 200, function() {
+  });
+  return false;
+});
+
+$('#services .service:nth-child(4) .service-icon').click(function() {
+  $(this).animate({
+    left: '-=250',
+  }, 200, function() {
+  });
+  $('#services .service:nth-child(4) .service-description').animate({
+    left: '-=350',
+  }, 200, function() {
+  });
+  return false;
+});
+
+$('#services .service:nth-child(4) .service-description').click(function() {
+  $(this).animate({
+    left: '+=350',
+  }, 200, function() {
+  });
+  $('#services .service:nth-child(4) .service-icon').animate({
+    left: '+=250',
+  }, 200, function() {
+  });
+  return false;
+});
+
+$('#services .service:nth-child(5) .service-icon').click(function() {
+  $(this).animate({
+    left: '-=250',
+  }, 200, function() {
+  });
+  $('#services .service:nth-child(5) .service-description').animate({
+    left: '-=350',
+  }, 200, function() {
+  });
+  return false;
+});
+
+$('#services .service:nth-child(5) .service-description').click(function() {
+  $(this).animate({
+    left: '+=350',
+  }, 200, function() {
+  });
+  $('#services .service:nth-child(5) .service-icon').animate({
+    left: '+=250',
+  }, 200, function() {
+  });
+  return false;
+});
+
+$('#services .service:nth-child(6) .service-icon').click(function() {
+  $(this).animate({
+    left: '-=250',
+  }, 200, function() {
+  });
+  $('#services .service:nth-child(6) .service-description').animate({
+    left: '-=350',
+  }, 200, function() {
+  });
+  return false;
+});
+
+$('#services .service:nth-child(6) .service-description').click(function() {
+  $(this).animate({
+    left: '+=350',
+  }, 200, function() {
+  });
+  $('#services .service:nth-child(6) .service-icon').animate({
+    left: '+=250',
+  }, 200, function() {
+  });
+  return false;
+});
+
+
+});
